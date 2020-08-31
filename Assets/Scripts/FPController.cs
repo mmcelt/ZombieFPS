af353becc6ec075198f6961f31d14aea7635227c
Ron Welch <mmcelt@yahoo.com>
2020-08-31 17:00:01 -0600

    Section 3 - CREATING A FIRST PERSON CHARACTER WITH CONTROLLER: 16 - Programming FPC Movement

---
+++ Assets/Scripts/FPController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPController : MonoBehaviour
+{
+	#region Fields
+
+	[SerializeField] float _speed = 0.1f;
+
+	#endregion
+
+	#region MonoBehaviour Methods
+
+	void Start() 
+	{
+		
+	}
+	
+	void Update() 
+	{
+		
+	}
+
+	void FixedUpdate()
+	{
+		float x = Input.GetAxis("Horizontal");
+		float z = Input.GetAxis("Vertical");
+
+		transform.position += new Vector3(x * _speed, 0f, z * _speed);
+	}
+	#endregion
+
+	#region Public Methods
+
+
+	#endregion
+
+	#region Private Methods
+
+
+	#endregion
+}
